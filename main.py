858cb038ce374c8ded7533b995e82f88dc8e5fd3
meetamna8 <meetamna8@gmail.com>
2024-06-13 23:54:02 +0500

    commit

---
+++ main.py
@@ -0,0 +1,43 @@
+# pip (pip is a package manager)
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get("https://www.geeksforgeeks.org/")
+
+soup = BeautifulSoup(req.content , "html.parser")
+res = soup.title
+
+print(soup.prettify())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
